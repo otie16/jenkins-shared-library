5842a7f506bb6e995371f547903daebc5f28ee0b
Otobong Godwin <66527770+otie16@users.noreply.github.com>
2024-09-30 15:25:19 +0100

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,12 @@
 #!/user/bin/env groovy
 
 def call() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
+     echo "building the docker image..."
+    // Extracting the github credentials from jenkins 
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-repo-credentials', passwordVariable: 'PASS', usernameVariable: 'USER', )]){
+        sh 'docker build -t otobongedoho18361/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push otobongedoho18361/demo-app:jma-2.0'
+    } 
 }
+
