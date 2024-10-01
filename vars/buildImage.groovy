233f9daa58fe21c955044532e3350e0efaca122a
Otobong Godwin <66527770+otie16@users.noreply.github.com>
2024-10-01 11:54:52 +0100

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,12 @@
 #!/user/bin/env groovy
 
-def call() {
+def call(String imageName) {
      echo "building the docker image..."
     // Extracting the github credentials from jenkins 
     withCredentials([usernamePassword(credentialsId: 'dockerhub-repo-credentials', passwordVariable: 'PASS', usernameVariable: 'USER', )]){
-        sh 'docker build -t otobongedoho18361/demo-app:jma-2.0 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push otobongedoho18361/demo-app:jma-2.0'
+        sh "docker push $imageName"
     } 
 }
 
