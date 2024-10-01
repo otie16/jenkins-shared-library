679b66f1c48db918bb0f29286e8ad3c3abde1631
otie16 <biggieestone@gmail.com>
2024-10-01 12:33:47 +0100

    added helper functions

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,7 @@
 #!/user/bin/env groovy
 
-def call(String imageName) {
-     echo "building the docker image..."
-    // Extracting the github credentials from jenkins 
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-repo-credentials', passwordVariable: 'PASS', usernameVariable: 'USER', )]){
-        sh "docker build -t $imageName ."
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh "docker push $imageName"
-    } 
-}
+import com.example.Docker
 
+def call(String imageName) {
+    return new Docker(this).buildDockerImage(imageName)
+}
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/user/bin/env groovy
 
 def call(){
-     echo "building the applicaton for $BRANCH_NAME"
+     echo "building the applicaton for branch $BRANCH_NAME"
      sh 'mvn package'
 }
